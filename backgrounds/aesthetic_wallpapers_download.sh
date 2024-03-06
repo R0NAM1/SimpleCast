d8ce0083d75b017765bdd4b2059de65c8e8b653c
R0NAM1 <r0nam1@toasty.cafe>
2024-03-05 23:34:24 -0800

    Add more default wallpaper downloads

---
+++ backgrounds/aesthetic_wallpapers_download.sh
@@ -0,0 +1,39 @@
+# This script downloads high res pictures from the aesthetic-wallpapers github, follows already existing config
+# https://github.com/D3Ext/aesthetic-wallpapers
+
+echo "Downloading image 0"
+wget https://raw.githubusercontent.com/D3Ext/aesthetic-wallpapers/main/images/astronaut.jpg -O 0.jpg
+
+echo "Downloading image 1"
+wget https://raw.githubusercontent.com/D3Ext/aesthetic-wallpapers/main/images/beautiful.jpg -O 1.jpg
+
+echo "Downloading image 2"
+wget https://raw.githubusercontent.com/D3Ext/aesthetic-wallpapers/main/images/bunny.png -O 2.png
+
+echo "Downloading image 3"
+wget https://raw.githubusercontent.com/D3Ext/aesthetic-wallpapers/main/images/colors.jpg -O 3.jpg
+
+# Saves as jpg but is png, should be fine.
+echo "Downloading image 4"
+wget https://raw.githubusercontent.com/D3Ext/aesthetic-wallpapers/main/images/ocean_with_cloud.png -O 4.jpg
+
+echo "Downloading image 5"
+wget https://raw.githubusercontent.com/D3Ext/aesthetic-wallpapers/main/images/cliff-edge.jpg -O 5.jpg
+
+echo "Downloading image 6"
+wget https://raw.githubusercontent.com/D3Ext/aesthetic-wallpapers/main/images/cat.jpg -O 6.jpg
+
+echo "Downloading image 7"
+wget https://raw.githubusercontent.com/D3Ext/aesthetic-wallpapers/main/images/girl.jpg -O 7.jpg
+
+echo "Downloading image 8"
+wget https://raw.githubusercontent.com/D3Ext/aesthetic-wallpapers/main/images/forest.png -O 8.jpg
+
+echo "Downloading image 9"
+wget https://raw.githubusercontent.com/D3Ext/aesthetic-wallpapers/main/images/fantasy-woods.jpg -O 9.jpg
+
+echo "Downloading image 10"
+wget https://raw.githubusercontent.com/D3Ext/aesthetic-wallpapers/main/images/gray-background.png -O 10.jpg
+
+echo "Downloading image 11"
+wget https://raw.githubusercontent.com/D3Ext/aesthetic-wallpapers/main/images/hands.jpg -O 11.jpg
