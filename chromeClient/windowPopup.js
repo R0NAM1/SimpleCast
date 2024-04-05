7a64a11b941c091a58356b4e075977376281b280
R0NAM1 <r0nam1@toasty.cafe>
2024-04-04 19:19:19 -0700

    Fix browser to chrome syntax

--- chromeClient/windowPopup.js
+++ chromeClient/windowPopup.js
@@ -1,17 +1,17 @@
 let extensionWindowId = null; // Store the window ID of your extension's window
 
-browser.action.onClicked.addListener(() => {
+chrome.action.onClicked.addListener(() => {
  // Check if the extension's window is already open
- browser.windows.getAll({}).then(windows => {
+ chrome.windows.getAll({}).then(windows => {
     const extensionWindow = windows.find(window => window.id === extensionWindowId);
 
     if (extensionWindow) {
       // If the window is open, close it
-      browser.windows.remove(extensionWindow.id);
+      chrome.windows.remove(extensionWindow.id);
       extensionWindowId = null; // Reset the window ID
     } else {
       // If the window is not open, open a new one
-      browser.windows.create({
+      chrome.windows.create({
         url: chrome.runtime.getURL("client.html"),
         type: "popup",
         width: 400, // Desired width
